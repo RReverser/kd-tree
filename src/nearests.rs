b6635f360494725eba4e23124d18c41689e4983a
Ingvar Stepanyan <me@rreverser.com>
2025-02-11 02:23:06 +0000

    Arcane micro optimisation

--- src/nearests.rs
+++ src/nearests.rs
@@ -48,45 +48,63 @@ pub fn kd_nearests<'a, T: KdPoint, V: VecLike<Item = ItemAndDistance<'a, T>>>(
         nearests: &mut V,
         kdtree: &'a [T],
         query: &T,
-        axis: usize,
+        mut axis: usize,
     ) {
-        let Some((mut before, item, mut after)) = split_at_mid(kdtree) else {
-            return;
-        };
-        unsafe {
-            assert_unchecked(axis < T::DIM);
-        }
-        let diff = query.at(axis) - item.at(axis);
-        if diff.is_positive() {
-            std::mem::swap(&mut before, &mut after);
-        }
-        let mut next_axis = axis + 1;
-        if next_axis == T::DIM {
-            next_axis = 0;
-        }
-        recurse(nearests, before, query, next_axis);
-        let distance_metric = item.distance_metric(query);
-        let i = nearests
-            .iter()
-            .rposition(move |item| item.distance_metric <= distance_metric)
-            .map_or(0, |i| i + 1);
-        if i < nearests.capacity() {
-            nearests.truncate(nearests.capacity() - 1);
-            nearests.insert(
-                i,
-                ItemAndDistance {
-                    item,
-                    distance_metric,
-                },
-            );
-        }
-        if !after.is_empty()
-            && nearests.get(nearests.capacity() - 1).map_or(true, |max| {
-                T::from_distance_to_metric(diff) < max.distance_metric
-            })
-        {
-            recurse(nearests, after, query, next_axis);
+        match split_at_mid(kdtree) {
+            None => {}
+            Some(([], item, [])) => {
+                insert_nearests(
+                    nearests,
+                    ItemAndDistance {
+                        item,
+                        distance_metric: query.distance_metric(item),
+                    },
+                );
+            }
+            Some((mut before, item, mut after)) => {
+                unsafe {
+                    assert_unchecked(axis < T::DIM);
+                }
+                let diff = query.at(axis) - item.at(axis);
+                if diff.is_positive() {
+                    std::mem::swap(&mut before, &mut after);
+                }
+                axis += 1;
+                if axis == T::DIM {
+                    axis = 0;
+                }
+                recurse(nearests, before, query, axis);
+                insert_nearests(
+                    nearests,
+                    ItemAndDistance {
+                        item,
+                        distance_metric: query.distance_metric(item),
+                    },
+                );
+                if !after.is_empty()
+                    && nearests.get(nearests.capacity() - 1).map_or(true, |max| {
+                        T::from_distance_to_metric(diff) < max.distance_metric
+                    })
+                {
+                    recurse(nearests, after, query, axis);
+                }
+            }
         }
     }
+
     recurse(nearests, kdtree, query, 0);
+}
+
+fn insert_nearests<'a, T: KdPoint, V: VecLike<Item = ItemAndDistance<'a, T>>>(
+    nearests: &mut V,
+    new_item: ItemAndDistance<'a, T>,
+) {
+    let i = nearests
+        .iter()
+        .rposition(|item| item.distance_metric <= new_item.distance_metric)
+        .map_or(0, |i| i + 1);
+    if i < nearests.capacity() {
+        nearests.truncate(nearests.capacity() - 1);
+        nearests.insert(i, new_item);
+    }
 }
