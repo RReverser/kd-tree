22548a84f6e868d611a316899a50771d4eddbcd7
Ingvar Stepanyan <me@rreverser.com>
2025-02-14 23:58:44 +0000

    Add nearests_all benchmark

--- benches/callgrind.rs
+++ benches/callgrind.rs
@@ -31,9 +31,16 @@ fn knn_graph(points: KdTree<XYZCoord, Vec<XYZCoord>>) {
     });
 }
 
+#[library_benchmark(setup = build_kdtree)]
+#[bench::small(1_000)]
+#[bench::large(1_000_000)]
+fn knn_graph_all(points: KdTree<XYZCoord, Vec<XYZCoord>>) {
+    black_box(points.nearests_all::<4>());
+}
+
 library_benchmark_group!(
     name = benches;
-    benchmarks = build, knn_graph
+    benchmarks = build, knn_graph, knn_graph_all
 );
 
 main!(library_benchmark_groups = benches);
